50b16356b63cc88812f04945cdd9a41bcac566e1
leducminh1411 <led56955@gmail.com>
2023-07-23 11:01:57 +0700

    leu leu

---
+++ baitap3/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void introduction()
+{
+    printf("Hi\n");
+    printf("ai em i Minh\n");
+    printf("day la bai tap ham\n");
+    printf("Mong 1 lan chay luon\n");
+
+}
+int main()
+{
+    introduction();
+    return 0;
+
+
+}
