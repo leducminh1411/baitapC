ddcd93225f317204f8c6deb1a5e753ab10f09fb5
leducminh1411 <led56955@gmail.com>
2023-07-23 12:08:34 +0700

    baitap3

--- baitap3/main.c
+++ baitap3/main.c
@@ -1,16 +1,36 @@
 #include<stdio.h>
-void introduction()
-{
-    printf("Hi\n");
-    printf("ai em i Minh\n");
-    printf("day la bai tap ham\n");
-    printf("Mong 1 lan chay luon\n");
+#include<math.h>
+/* Tiền khai báo */
+// hàm không có tham số truyền vào
+// Hàm tính cạnh huyền tam giác vuông với tham số truyền vào là 2 cạnh góc vuông
+/** Gợi ý
+ * Công thức tính căn bậc 2 trong c 
+ * Biến toàn cục sẽ là 2 cạnh góc vuông
+ * Biến cục bộ dc nằm trong function tính cạnh huyền
+ * */
 
+// Hàm trả về giá trị cạnh huyền của tam giác vuông.
+/** Gợi ý
+ * 
+ * Biến toàn cục sẽ là 2 cạnh góc vuông
+ * Biến cục bộ sẽ là cạnh huyền
+ * Biến cục bộ dc nằm trong function main()
+ * */
+float addition( int a, int b)
+{
+    float sum;
+    //sum = a * a;
+   // printf ("%f", sum);
+    sum = sqrt ( pow(a,2) + pow(b,2));
+    return sum;
 }
 int main()
 {
-    introduction();
-    return 0;
-
-
+    float minh = addition(2,3);
+    printf ("%f", minh);
+    return 0;   
 }
+
+
+
+    
